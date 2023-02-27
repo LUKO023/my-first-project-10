c5d180fc5934227c78ea4168a8168d866dd2cdb9
LUKO023 <lukas.kokott@arvato-scs.com>
2023-02-27 14:52:28 +0100

    new

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,97 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('my-first-project');
   });
 
-  it('should render title', () => {
+  it(`should sum up two numbers'`, () => {
+    //given
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first-project app is running!');
+    const app = fixture.componentInstance; 
+     app.storeArray = ['5', '+', '1'];
+    //when
+    app.result();
+    
+    //then
+    expect(app.num).toEqual(6)
+
   });
+  it(`should substract two numbers'`, () => {
+    //given
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance; 
+     app.storeArray = ['5', '-', '1'];
+    //when
+    app.result();
+    
+    //then
+    expect(app.num).toEqual(4)
+
+  });
+
+  it(`should multiplicate two numbers'`, () => {
+    //given
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance; 
+     app.storeArray = ['5', '*', '2'];
+    //when
+    app.result();
+    
+    //then
+    expect(app.num).toEqual(10)
+
+  });
+  it(`should divide two numbers'`, () => {
+    //given
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance; 
+     app.storeArray = ['5', '/', '2'];
+    //when
+    app.result();
+    
+    //then
+    expect(app.num).toEqual(2.5)
+
+  });
+  it(`should calculate number chain'`, () => {
+    //given
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance; 
+     app.storeArray = ['3', '+', '6', '*', '7', '-', '4', '/', '9', '+', '100', '/', '6'];
+    //when
+    app.result();
+    
+    //then
+    expect(app.num).toEqual(17.75925925925926)
+
+  });
+  it(`should calculate number chain with dot'`, () => {
+    //given
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance; 
+     app.storeArray = ['3.6', '*', '5', '-', '3.7', '+', '8.66'];
+    //when
+    app.result();
+    
+    //then
+    expect(app.num).toEqual(22.96)
+
+  });
+
+
+
+
+
+  it(`should give error'`, () => {
+    //given
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.storeArray = ['3', '+', '3', '+', '+', '5'];
+    //when
+    app.operatorCheck();
+    
+    //then
+    expect(app.error).toEqual(true)
+
+  });
+
+
+
 });
